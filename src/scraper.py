0b0ee0c9b1393f880fae0f6fe61406126a17f3dc
Paula Iacoban <i.p.iacoban@gmail.com>
2021-05-01 20:19:30 +0200

    Log request status

--- src/scraper.py
+++ src/scraper.py
@@ -1,15 +1,32 @@
+import csv
+import os
 import scrapy
+import pickle
 
+urls_file_path = os.getcwd() + "/src/top-1m.csv"
+results_file_path = os.getcwd() + "/src/res.csv"
 
-class BrickSetSpider(scrapy.Spider):
-    name = "brickset_spider"
-    start_urls = ['http://brickset.com/sets/year-2016']
+def get_top_websites(n):
+    urls = []
+    with  open(urls_file_path) as csv_file:
+        reader = csv.reader(csv_file)
+        for i, line in enumerate(reader):
+            if i>=n:
+                return urls
+            urls.append('http://' + line[1])
 
+        return urls
+
+def append_list_as_row(list_of_elem):
+    # Open file in append mode
+    with open(results_file_path, 'a+', newline='') as write_obj:
+        # Create a writer object from csv module
+        csv_writer = csv.writer(write_obj)
+        # Add contents of list as last row in the csv file
+        csv_writer.writerow(list_of_elem)
+
+class SpiderBot(scrapy.Spider):
+    name = "spiderbot"
+    start_urls = get_top_websites(500)
     def parse(self, response):
-        SET_SELECTOR = '.set'
-        for brickset in response.css(SET_SELECTOR):
-
-            NAME_SELECTOR = 'h1 ::text'
-            yield {
-                'name': brickset.css(NAME_SELECTOR).extract_first(),
-            }
+        append_list_as_row([response.status, response.url])
