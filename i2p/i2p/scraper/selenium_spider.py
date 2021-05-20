fc7d8e4369a87e93140a18ded0026e50927e1a9a
Paula Iacoban <i.p.iacoban@gmail.com>
2021-05-20 13:53:24 +0200

    Update selenium  for subdomains

--- i2p/i2p/scraper/selenium_spider.py
+++ i2p/i2p/scraper/selenium_spider.py
@@ -6,6 +6,7 @@ import time
 import datetime
 import importlib
 import time
+import re
 import urllib as url
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -23,39 +24,74 @@ stats = folder_path + "logs/stats_public.csv"
 screenshot_pub = folder_path + "screenshots/pub/"
 screenshot_i2p = folder_path + "screenshots/i2p/"
 
-n = 300
-start_from = 200
-start_urls = util.get_top_websites(n, start_from)
+def success(site):
+    if (re.search("^2..", site) or re.search("^3..", site)):
+        return True
+    return False   
+
+
+def get_sites(n, start_from=0):
+    # Parse urls.csv file
+    urls = []
+    folder_path = os.getcwd() + "/i2p/i2p/scraper/"
+# urls_file_path = folder_path + "urls.csv"
+    urls_file_path = folder_path + "crawled_urls.csv"
+    with open(urls_file_path) as csv_file:
+        reader = csv.reader(csv_file)
+        for i, line in enumerate(reader):
+            if i>=n:
+                return urls
+            if i>=start_from:
+                domains = []
+                for j in range(len(line)):
+                    domains.append(line[j])
+                urls.append(domains)
+
+        return urls
+
+n = 500
+start_from = 0
+# start_urls = util.get_top_websites(n, start_from)
+start_urls = get_sites(n, start_from)
 PROXY = "127.0.0.1:4444"
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--proxy-server=%s' % PROXY)
+# chrome_options.add_extension(folder_path+"extentions/ad-block.crx")
+chrome_options.add_extension(folder_path+"extentions/cookie.crx")
+
+
+chrome_options_pub = webdriver.ChromeOptions()
+# chrome_options_pub.add_extension(folder_path+"extentions/ad-block.crx")
+chrome_options_pub.add_extension(folder_path+"extentions/cookie.crx")
 
 def run_spider():
-    i = start_from
+    main_domain = start_from
     for url in start_urls:
-        with webdriver.Chrome(options=chrome_options) as i2p_driver:
-            try:
-                save = screenshot_i2p + str(i) +'i2p.png'
-                i2p_driver.get(url)
-                time.sleep(2)
-                i2p_driver.save_screenshot(save)
+        sub_domain = 0
+        for link in url:
+            with webdriver.Chrome(options=chrome_options) as i2p_driver:
+                try:
+                    save = screenshot_i2p + str(main_domain)+"_"+ str(sub_domain)+'i2p.png'
+                    i2p_driver.get(link)
+                    time.sleep(1)
+                    i2p_driver.save_screenshot(save)
+                
+                except TimeoutException:
+                    print('i2p timeout on ' + str(main_domain)+"_"+ str(sub_domain))
             
-            except TimeoutException:
-                print('i2p timeout on ' + str(i))
+            with webdriver.Chrome(options=chrome_options_pub) as driver:
+                try:
+                    save = screenshot_pub + str(main_domain)+"_"+ str(sub_domain) +'pub.png'
+                    driver.get(link)
+                    time.sleep(1)
+                    driver.save_screenshot(save)
         
-        with webdriver.Chrome() as driver:
-            try:
-                save = screenshot_pub + str(i) +'pub.png'
-                driver.get(url)
-                time.sleep(2)
-                driver.save_screenshot(save)
+                except TimeoutException:
+                    print('pub timeout on ' + str(main_domain)+"_"+ str(sub_domain))
+            
+            sub_domain += 1
         
-            except TimeoutException:
-                print('pub timeout on ' + str(i))
-        
-        i += 1
-        
+        main_domain += 1
 
-        
 
 run_spider()
